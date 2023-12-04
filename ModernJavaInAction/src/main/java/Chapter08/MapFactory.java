41bfb45b6d6a2376e65b8833bb811d8a978fb625
ForExample <jyh6197@gmail.com>
2023-12-05 00:03:09 +0900

    feat: Map, List Factory
    Map.ofEntries
    List.of is immutable

---
+++ ModernJavaInAction/src/main/java/Chapter08/MapFactory.java
@@ -0,0 +1,12 @@
+package Chapter08;
+
+import java.util.Map;
+
+public class MapFactory {
+    public static void main(String[] args) {
+        Map<String, Integer> ofEntries = Map.ofEntries(Map.entry("Raphael", 30),
+                Map.entry("Olivia", 25),
+                Map.entry("Thibaut", 26));
+        System.out.println("ofEntries = " + ofEntries);
+    }
+}
