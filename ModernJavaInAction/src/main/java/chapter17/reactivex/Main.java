6f5e0c7aa1e0dbb9bb9efb78ddabb85968e9c1e3
ForExample <jyh6197@gmail.com>
2023-12-11 00:52:54 +0900

    feat: Improve Reactive Sample Using RxJava

--- ModernJavaInAction/src/main/java/chapter17/reactivex/Main.java
+++ ModernJavaInAction/src/main/java/chapter17/reactivex/Main.java
@@ -4,11 +4,13 @@ package chapter17.reactivex;
 import chapter17.TempInfo;
 import io.reactivex.rxjava3.core.Observable;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        Observable<TempInfo> observable = getTemperature("NewYork");
+//        Observable<TempInfo> observable = getNegativeTemperature("NewYork");
+        Observable<TempInfo> observable = getCelsiusTemperatures("NewYork", "Chicago", "SanFrancisco");
         observable.blockingSubscribe(new TempObserver());
     }
     public static Observable<TempInfo> getTemperature(String town) {
@@ -27,5 +29,21 @@ public class Main {
                                 }
                             }
                         }));
+    }
+
+    public static Observable<TempInfo> getCelsiusTemperature(String town) {
+        return getTemperature(town)
+                .map(tempInfo -> new TempInfo(tempInfo.getTown(), (tempInfo.getTemp() - 32) * 5 / 9));
+    }
+
+    public static Observable<TempInfo> getCelsiusTemperatures(String... towns) {
+        return Observable.merge(Arrays.stream(towns)
+                .map(Main::getCelsiusTemperature)
+                .toList());
+    }
+
+    public static Observable<TempInfo> getNegativeTemperature(String town) {
+        return getCelsiusTemperature(town)
+                .filter(tempInfo -> tempInfo.getTemp() < 0);
     }
 }
